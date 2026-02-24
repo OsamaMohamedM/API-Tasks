c7fb814cb6670d8bdda2a91e98a78eaca4017a82
osama <osamamoahmed242@gmail.com>
2026-02-24 03:33:44 +0200

    feat: Add Doctor/Patient models and integrate Swagger UI
    Added Doctor and Patient model classes with validation and relationships. Integrated Swagger (Swashbuckle) for API documentation and testing, updated launch settings to open Swagger UI by default, and added necessary NuGet packages.

---
+++ Task1/Task1/Models/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Patient> Patients { get; set; }
+    }
+}
---
+++ Task1/Task1/Models/Patient.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task1.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string PhoneNumber { get; set; }
+
+        public DateTime CheckInDate { get; set; }
+
+        public Doctor Doctor { get; set; }
+
+        [ForeignKey(nameof(Doctor))]
+        public int DoctorId { get; set; }
+
+        public bool isRegistered { get; set; }
+    }
+}
